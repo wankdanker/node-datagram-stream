5371d24c2801864a6510e4a415426d532bc9e1fd
Dan VerWeire <dverweire@gmail.com>
2014-10-01 23:56:00 -0400

    Add test using tape

--- test.js
+++ test.js
@@ -1,4 +1,48 @@
-var udpStream = require('./');
+var udpStream = require('./')
+    , test = require('tape');
 
-process.stdin.pipe(udpStream({ broadcast : '255.255.255.255', port : '61616' })).pipe(process.stdout);
+test('broadcast destination', function (t) {
+    t.plan(1);
+
+    var s = udpStream({ broadcast : '255.255.255.255', port : '61616' })
+    
+    s.on('data', function (chunk) {
+        t.equal('hello', chunk.toString());
+        t.end();
+
+	s.end();
+    });
+
+    s.write('hello');
+});
+
+test('multicast destination', function (t) {
+    t.plan(1);
+
+    var s = udpStream({ multicast : '239.5.5.5', port : '61616', loopback : true })
+    
+    s.on('data', function (chunk) {
+        t.equal('hello', chunk.toString());
+        t.end();
+
+	s.end();
+    });
+
+    s.write('hello');
+});
+
+test('unicast destination', function (t) {
+    t.plan(1);
+
+    var s = udpStream({ unicast : '127.0.0.1', port : '61616' })
+    
+    s.on('data', function (chunk) {
+        t.equal('hello', chunk.toString());
+        t.end();
+
+	s.end();
+    });
+
+    s.write('hello');
+});
 
