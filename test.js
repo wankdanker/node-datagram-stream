adf55a06a9a2149a626a2eaf91c7cd4cb7e2a1bd
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2015-04-07 13:27:05 +0200

    Fix unit tests on node 0.10.x

--- test.js
+++ test.js
@@ -4,13 +4,13 @@ var udpStream = require('./')
 test('broadcast destination', function (t) {
     t.plan(1);
 
-    var s = udpStream({ broadcast : '255.255.255.255', port : '61616' })
+    var s = udpStream({ broadcast : '255.255.255.255', port : 61616 });
     
     s.on('data', function (chunk) {
         t.equal('hello', chunk.toString());
         t.end();
 
-	s.end();
+        s.end();
     });
 
     s.write('hello');
@@ -19,13 +19,13 @@ test('broadcast destination', function (t) {
 test('multicast destination', function (t) {
     t.plan(1);
 
-    var s = udpStream({ multicast : '239.5.5.5', port : '61616', loopback : true })
+    var s = udpStream({ multicast : '239.5.5.5', port : 61616, loopback : true });
     
     s.on('data', function (chunk) {
         t.equal('hello', chunk.toString());
         t.end();
 
-	s.end();
+        s.end();
     });
 
     s.write('hello');
@@ -34,13 +34,13 @@ test('multicast destination', function (t) {
 test('unicast destination', function (t) {
     t.plan(1);
 
-    var s = udpStream({ unicast : '127.0.0.1', port : '61616' })
+    var s = udpStream({ unicast : '127.0.0.1', port : 61616, address: '127.0.0.1' });
     
     s.on('data', function (chunk) {
         t.equal('hello', chunk.toString());
         t.end();
 
-	s.end();
+        s.end();
     });
 
     s.write('hello');
